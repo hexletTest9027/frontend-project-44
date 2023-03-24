557ec8c0d2021e79cef231c5c0d68f7920653e5c
Sergei_jobLab <sergey2003.k.96@gmail.com>
2023-03-24 12:13:36 +0300

    last game

---
+++ src/games/brain-prime.js
@@ -0,0 +1,19 @@
+import random from '../utils/utils.js';
+
+const isPrime = (num) => {
+  for (let i = 2, s = Math.sqrt(num); i <= s; i += 1) {
+    if (num % i === 0) return false;
+  }
+  return num > 1;
+};
+
+const brainPime = () => {
+  const number = random(1, 100);
+  return ({
+    task: 'Answer "yes" if given number is prime. Otherwise answer "no".',
+    question: number,
+    rightAnswer: isPrime(number) ? 'yes' : 'false',
+  });
+};
+
+export default brainPime;
