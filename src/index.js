f703bf3f1c85b87c0a58f33a565959764fcb30dd
Sergei_jobLab <sergey2003.k.96@gmail.com>
2023-05-10 13:59:00 +0300

    FIX

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ export default function startGame(game) {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-  let hasError = false;
   const { task } = game();
   console.log(task);
   for (let i = 0; i < 3; i += 1) {
@@ -14,10 +13,8 @@ export default function startGame(game) {
     if (answer.toString() === rightAnswer.toString()) {
       console.log('Correct!');
     } else {
-      hasError = true;
       console.log(`${answer} is wrong answer ;(. Correct answer was ${rightAnswer}.Let's try again, ${name}!`);
       return;
     }
   }
-  console.log(!hasError ? `Congratulations, ${name}!` : `Let's try again, ${name}!`);
 }
